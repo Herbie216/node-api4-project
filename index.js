b642a54ccfe29443602093f7c3ccfdedb4c0498f
Herbie216 <150303988+Herbie216@users.noreply.github.com>
2024-05-29 02:42:13 -0400

    wip

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 const express = require('express')
-const dotenv = require('dotenv');
+require('dotenv').config();
 const cors = require('cors');
 
-dotenv.config();
-
-const port = process.env.PORT || 9000;
+//console.log(process.env.PORT, process.env.NODE)
+const PORT = process.env.PORT || 9000;
 
 const server = express()
 
 server.use(cors());
 server.use(express.json())
 
-let users = [];
+// let users = [];
 
-server.get('/api/users', (req, res) => {
-    res.json(users);
-  });
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'api is working'});
+});
 
-  server.post('/api/register', (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      return res.status(400).json({ message: 'Username and password are required.' });
-    }
-    const newUser = { id: users.length + 1, username, password };
-    users.push(newUser);
-    res.status(201).json(newUser);
-  });
-  
-  server.post('/api/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(user => user.username === username && user.password === password);
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid username or password.' });
-    }
-    res.json({ message: `Welcome, ${username}!` });
-  });
+// server.get('/api/users', (req, res) => {
+//     res.json(users);
+// });
 
-server.listen(port, (req, res) => {
-    console.log(`Listening on port ${port}`)
+// server.post('/api/register', (req, res) => {
+//     const { username, password } = req.body;
+//     if (!username || !password) {
+//         return res.status(400).json({ message: 'Username and password are required.' });
+//     }
+//     const newUser = { id: users.length + 1, username, password };
+//     users.push(newUser);
+//     res.status(201).json(newUser);
+// });
+
+// server.post('/api/login', (req, res) => {
+//     const { username, password } = req.body;
+//     const user = users.find(user => user.username === username && user.password === password);
+//     if (!user) {
+//         return res.status(401).json({ message: 'Invalid username or password.' });
+//     }
+//     res.json({ message: `Welcome, ${username}!` });
+// });
+
+server.use('*', (req, res, next) => {
+    res.send(`<h1>Hello, there.</h1>`)
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
